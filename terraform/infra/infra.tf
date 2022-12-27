62e0bf375eecd0fa741b49ba6c2753063a9529fa
James Lok <jameslok@Jamess-MacBook-Pro.local>
2022-12-27 23:21:04 +0000

    TDC-5 added api gateway

--- terraform/infra/infra.tf
+++ terraform/infra/infra.tf
@@ -67,3 +67,79 @@ resource "aws_lambda_function" "airport_endpoint_function" {
   runtime          = "python3.9"
   source_code_hash = filebase64sha256("lambda_function.zip")
 }
+
+resource "aws_api_gateway_rest_api" "airport_apigw" {
+  name        = "airport_apigw"
+  description = "Airport API Gateway"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+resource "aws_api_gateway_resource" "airport" {
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  parent_id   = aws_api_gateway_rest_api.airport_apigw.root_resource_id
+  path_part   = "airport"
+}
+resource "aws_api_gateway_method" "readallairports" {
+  rest_api_id   = aws_api_gateway_rest_api.airport_apigw.id
+  resource_id   = aws_api_gateway_resource.airport.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+# apigateway lambda function integration 
+resource "aws_api_gateway_integration" "airport-lambda" {
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  resource_id = aws_api_gateway_method.readallairports.resource_id
+  http_method = aws_api_gateway_method.readallairports.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri = aws_lambda_function.airport_endpoint_function.invoke_arn
+
+  request_templates = {
+    "application/json" = "{ \"statusCode\": 200 }"
+  }
+}
+resource "aws_lambda_permission" "apigw-airport_endpoint_function" {
+  action        = "lambda:InvokeFunction"
+  statement_id  = "AllowAirportInvoke"
+  function_name = aws_lambda_function.airport_endpoint_function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.airport_apigw.execution_arn}/*/*/*"
+}
+resource "aws_api_gateway_deployment" "productapistageprod" {
+  depends_on = [
+    aws_api_gateway_integration.airport-lambda
+  ]
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  stage_name  = "prod"
+}
+resource "aws_api_gateway_integration_response" "myintegrationresponse" {
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  resource_id = aws_api_gateway_resource.airport.id
+  http_method = aws_api_gateway_method.readallairports.http_method
+  status_code = "200"
+
+  response_templates = {
+    "application/json" = ""
+  } 
+
+  depends_on = [
+    aws_api_gateway_integration.airport-lambda,
+    aws_api_gateway_method_response.mymethodresponse
+  ]
+}
+resource "aws_api_gateway_method_response" "mymethodresponse" {
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  resource_id = aws_api_gateway_resource.airport.id
+  http_method = aws_api_gateway_method.readallairports.http_method
+  status_code = 200
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  depends_on = [
+    aws_api_gateway_method.readallairports,
+  ]
+}
