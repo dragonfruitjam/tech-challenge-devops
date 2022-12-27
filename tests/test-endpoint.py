4209b547039032bd7b84face48e5caaace49ed76
James Lok <jameslok@Jamess-MacBook-Pro.local>
2022-12-27 11:51:01 +0000

    TDC-3 added unit tests for airport endpoints

---
+++ tests/test-endpoint.py
@@ -0,0 +1,32 @@
+import unittest
+import requests
+
+class TestEndpoints(unittest.TestCase):     
+    # Tests users only can access correct endpoints.
+    def test_airport_endpoint_successs(self):
+        res = requests.get('http://127.0.0.1:8080/airport', timeout=5)
+        self.assertEqual( 200, res.status_code )
+
+    def test_airport_endpoint_failure(self):
+        res = requests.get('http://127.0.0.1:8080/airports', timeout=5)
+        self.assertEqual( 501, res.status_code )
+
+    def test_get_airport_successs(self):
+        res = requests.get('http://127.0.0.1:8080/airport/AMS', timeout=5)
+        self.assertEqual( 200, res.status_code )
+
+    def test_get_airport_failure(self):
+        res = requests.get('http://127.0.0.1:8080/airport/ABC', timeout=5)
+        self.assertEqual( 500, res.status_code )
+
+    def test_connection_endpoint_success(self):
+        res = requests.get('http://127.0.0.1:8080/airport/AMS/to/ATH', timeout=5)
+        self.assertEqual( 200, res.status_code )
+    
+    def test_connection_endpoint_failure(self):
+        res = requests.get('http://127.0.0.1:8080/airport/ABC/to/DEF', timeout=5)
+        self.assertEqual( 404, res.status_code )
+
+        
+if __name__ == '__main__':
+    unittest.main()
