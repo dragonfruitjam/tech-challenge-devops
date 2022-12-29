7a8b003521d14004b2b8342220d5446f5cfab5b6
dragonfruitjam <120990075+dragonfruitjam@users.noreply.github.com>
2022-12-29 07:52:03 +0000

    Tdc 5 deploy api gateway using terraform (#5)
    * TDC-5 added api gateway
    Co-authored-by: James Lok <jameslok@Jamess-MacBook-Pro.local>

--- terraform/create-aws-infra.sh
+++ terraform/create-aws-infra.sh
@@ -4,4 +4,10 @@ cd ../lambda ; zip -r ../terraform/infra/lambda_function.zip . ; cd ../terraform
 cd infra
 terraform init
 terraform apply -auto-approve
+
+retVal=$?
+if [ $retVal -eq 1 ]; then
+    exit $retVal
+fi
+
 docker run -v ${HOME}/.aws/:/root/.aws/:ro db-initiator
--- terraform/infra/infra.tf
+++ terraform/infra/infra.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
+# dynamodb
 resource "aws_dynamodb_table" "airport" {
   name           = "airport"
   hash_key = "id"
@@ -14,6 +15,15 @@ resource "aws_dynamodb_table" "airport" {
   }
 }
 
+# lambda function 
+resource "aws_lambda_function" "airport_endpoint_function" {
+  filename         = "lambda_function.zip"
+  function_name    = "airport-endpoints"
+  role             = aws_iam_role.lambda_exec_role.arn
+  handler          = "handler_airport.lambda_handler"
+  runtime          = "python3.9"
+  source_code_hash = filebase64sha256("lambda_function.zip")
+}
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
 
@@ -33,7 +43,6 @@ resource "aws_iam_role" "lambda_exec_role" {
 }
 EOF
 }
-
 resource "aws_iam_role_policy" "lambda_dynamodb_policy" {
   name = "lambda_dynamodb_policy"
   role = aws_iam_role.lambda_exec_role.id
@@ -59,11 +68,78 @@ resource "aws_iam_role_policy" "lambda_dynamodb_policy" {
 EOF
 }
 
-resource "aws_lambda_function" "airport_endpoint_function" {
-  filename         = "lambda_function.zip"
-  function_name    = "airport-endpoints"
-  role             = aws_iam_role.lambda_exec_role.arn
-  handler          = "handler_airport.lambda_handler"
-  runtime          = "python3.9"
-  source_code_hash = filebase64sha256("lambda_function.zip")
+resource "aws_api_gateway_rest_api" "airport_apigw" {
+  name        = "airport_apigw"
+  description = "Airport API Gateway"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+resource "aws_api_gateway_resource" "airport" {
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  parent_id   = aws_api_gateway_rest_api.airport_apigw.root_resource_id
+  path_part   = "airport"
+}
+resource "aws_api_gateway_method" "readallairports" {
+  rest_api_id   = aws_api_gateway_rest_api.airport_apigw.id
+  resource_id   = aws_api_gateway_resource.airport.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+# apigateway lambda function integration 
+resource "aws_api_gateway_integration" "airport-lambda" {
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  resource_id = aws_api_gateway_method.readallairports.resource_id
+  http_method = aws_api_gateway_method.readallairports.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri = aws_lambda_function.airport_endpoint_function.invoke_arn
+
+  request_templates = {
+    "application/json" = "{ \"statusCode\": 200 }"
+  }
+}
+resource "aws_lambda_permission" "apigw-airport_endpoint_function" {
+  action        = "lambda:InvokeFunction"
+  statement_id  = "AllowAirportInvoke"
+  function_name = aws_lambda_function.airport_endpoint_function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.airport_apigw.execution_arn}/*/*/*"
+}
+resource "aws_api_gateway_deployment" "productapistageprod" {
+  depends_on = [
+    aws_api_gateway_integration.airport-lambda
+  ]
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  stage_name  = "prod"
+}
+resource "aws_api_gateway_integration_response" "myintegrationresponse" {
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  resource_id = aws_api_gateway_resource.airport.id
+  http_method = aws_api_gateway_method.readallairports.http_method
+  status_code = "200"
+
+  response_templates = {
+    "application/json" = ""
+  } 
+
+  depends_on = [
+    aws_api_gateway_integration.airport-lambda,
+    aws_api_gateway_method_response.mymethodresponse
+  ]
+}
+resource "aws_api_gateway_method_response" "mymethodresponse" {
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  resource_id = aws_api_gateway_resource.airport.id
+  http_method = aws_api_gateway_method.readallairports.http_method
+  status_code = 200
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  depends_on = [
+    aws_api_gateway_method.readallairports,
+  ]
 }
