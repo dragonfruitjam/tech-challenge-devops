5a8bfd011198bbf93de98dfb3d5eac4b55c32f63
James Lok <jameslok@Jamess-MacBook-Pro.local>
2022-12-31 19:25:23 +0000

    TDC-9 zip lambda function in github actions

---
+++ terraform/infra/remote.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    encrypt  = true
+    bucket   = "airport-api-tf"
+    key      = "infra.tfstate"
+    region   = "us-west-2"
+    dynamodb_table = "airportapitflock"
+  }
+}
