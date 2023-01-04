e8cd9afc944419dedb2be383b3ceb8f8a69c27c4
James Lok <jameslok@Jamess-MacBook-Pro.local>
2023-01-04 21:51:33 +0000

    TDC-12 copied files from lambda folder instead of folder itself

--- terraform/package-lambda-functions.sh
+++ terraform/package-lambda-functions.sh
@@ -1,15 +1,17 @@
+# this script packages the lambda funtions with the required packages so that they can be deployed to aws by terraform 
 # packages airport endpoint lambda function
 cd ../lambda ; zip -r ../terraform/infra/airport_lambda_function.zip . ; cd ../terraform
-
 # packages journey endpoint lambda function
 python3 -m venv ../lambda/.venv                             
 source ../lambda/.venv/bin/activate 
 pip3 install -r ../lambda/journey-requirements.txt
 mkdir ../temp
+
 cp -rf ../lambda/.venv/lib/*/site-packages/* ../temp
-cp -rf ../lambda/ ../temp
+cp -rf ../lambda/* ../temp
 rm -rf ../lambda/.venv
 
+# removes unneccessary files from lambda function folder
 rm ../temp/dockerfile
 rm ../temp/handler_as_app.py
 rm ../temp/handler_airport.py
