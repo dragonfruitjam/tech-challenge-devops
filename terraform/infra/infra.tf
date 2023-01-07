ecfbe7b8a21ef4dfe3f328a383634ebcb701f88a
James Lok <jameslok@Jamess-MacBook-Pro.local>
2023-01-07 22:41:51 +0000

    TDC-6 added vehicle endpont to api gateway

--- terraform/infra/infra.tf
+++ terraform/infra/infra.tf
@@ -60,7 +60,7 @@ resource "aws_iam_role_policy" "lambda_dynamodb_policy" {
 EOF
 }
 
-# lambda function 
+# airport lambda function 
 resource "aws_lambda_function" "airport_endpoint_function" {
   filename         = "airport_lambda_function.zip"
   function_name    = "airport-endpoints"
@@ -124,7 +124,8 @@ resource "aws_api_gateway_deployment" "productapistageprod" {
   depends_on = [
     aws_api_gateway_integration.airport-lambda,
     aws_api_gateway_integration.airport-lambda-single,
-    aws_api_gateway_integration.journey-lambda
+    aws_api_gateway_integration.journey-lambda,
+    aws_api_gateway_integration.vehicle-lambda
   ]
   rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
   stage_name  = "prod"
@@ -276,6 +277,86 @@ resource "aws_api_gateway_method_response" "journey-method-response" {
   ]
 }
 
+# apigateway vehicle lambda function integration 
+resource "aws_lambda_function" "vehicle_endpoint_function" {
+  filename         = "airport_lambda_function.zip"
+  function_name    = "vehicle-endpoints"
+  role             = aws_iam_role.lambda_exec_role.arn
+  handler          = "handler_vehicle.lambda_handler"
+  runtime          = "python3.9"
+  source_code_hash = filebase64sha256("airport_lambda_function.zip")
+}
+resource "aws_lambda_permission" "apigw-vehicle_endpoint_function" {
+  action        = "lambda:InvokeFunction"
+  statement_id  = "AllowVehicleInvoke"
+  function_name = aws_lambda_function.vehicle_endpoint_function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.airport_apigw.execution_arn}/*/*/*"
+}
+resource "aws_api_gateway_resource" "vehicle" {
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  parent_id   = aws_api_gateway_rest_api.airport_apigw.root_resource_id
+  path_part   = "vehicle"
+}
+resource "aws_api_gateway_resource" "no-of-people" {
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  parent_id   = aws_api_gateway_resource.vehicle.id
+  path_part   = "{numberOfPeople}"
+}
+resource "aws_api_gateway_resource" "distance" {
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  parent_id   = aws_api_gateway_resource.no-of-people.id
+  path_part   = "{distance}"
+}
+resource "aws_api_gateway_method" "cheapest-vehicle" {
+  rest_api_id   = aws_api_gateway_rest_api.airport_apigw.id
+  resource_id   = aws_api_gateway_resource.distance.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+resource "aws_api_gateway_integration" "vehicle-lambda" {
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  resource_id = aws_api_gateway_method.cheapest-vehicle.resource_id
+  http_method = aws_api_gateway_method.cheapest-vehicle.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri = aws_lambda_function.vehicle_endpoint_function.invoke_arn
+
+  request_templates = {
+    "application/json" = "{ \"statusCode\": 200 }"
+  }
+}
+resource "aws_api_gateway_integration_response" "vehicle-integration-response" {
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  resource_id = aws_api_gateway_resource.distance.id
+  http_method = aws_api_gateway_method.cheapest-vehicle.http_method
+  status_code = "200"
+
+  response_templates = {
+    "application/json" = ""
+  } 
+
+  depends_on = [
+    aws_api_gateway_integration.vehicle-lambda,
+    aws_api_gateway_method_response.vehicle-method-response
+  ]
+}
+resource "aws_api_gateway_method_response" "vehicle-method-response" {
+  rest_api_id = aws_api_gateway_rest_api.airport_apigw.id
+  resource_id = aws_api_gateway_resource.distance.id
+  http_method = aws_api_gateway_method.cheapest-vehicle.http_method
+  status_code = 200
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  depends_on = [
+    aws_api_gateway_method.cheapest-vehicle,
+  ]
+}
+
+# outputs url on command line
 output "airport_api_invoke_url" {
   value = aws_api_gateway_deployment.productapistageprod.invoke_url
 }
